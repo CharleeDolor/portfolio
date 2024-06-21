f2904f10969c3ba8b30b36adfbfa7f4d7950b424
Charlee_Dolor <dolorcharlee55@gmail.com>
2024-06-21 15:40:06 +0800

    initial commit

---
+++ resources/js/main.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    var $jumpingText = $('#jumpingText');
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {
+            $jumpingText.css({
+                'display': 'none'
+            });
+        } else {
+            $jumpingText.css({
+                'display': 'block'
+            });
+        }
+    });
+
+    $('.to-main-content').click(function () {
+        $('.main-content').toggleClass("slide-right");
+        $('#navigation').toggleClass("slide-right");
+
+        // show main content
+        $('.main-content').css({
+            'visibility': "visible"
+        });
+
+        // hide navigation
+        $('#navigation').css({
+            'visibility': "hidden"
+        });
+    });
+
+    $("#navigation-link").click(function () {
+        $('.main-content').toggleClass("slide-right");
+        $('#navigation').toggleClass("slide-right");
+
+        // hide main-content
+        $('.main-content').css({
+            'visibility': "hidden"
+        });
+
+        // show navigation
+        $('#navigation').css({
+            'visibility': "visible"
+        });
+    });
+
+    $('.tabs').tabs();
+    var slider = $('.slider').slider({
+        duration: 0,
+        interval: 6000
+    });
+
+    setTimeout(function () {
+        clearInterval(slider[0].M_Slider.interval);
+    }, 0);
+
+    $('.materialboxed').materialbox();
+    $(".dropdown-trigger").dropdown();
+    $('.scrollspy').scrollSpy();
+});
